2248e541bf2c31730040a7a4d2438a616b91c4cf
rhobbikhaidir <rhobbie09@gmail.com>
2022-06-03 09:57:18 +0700

    add costumHooks for API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-row px-12 mt-10">
+      <div className="border border-gray-400 w-1/4 mx-6 text-center">
+        <p>Perusahaan </p>
+        <p>Barang</p>
+      </div>
+      <div className="border border-gray-400 w-3/4">test</div>
+    </div>
+  );
 }
 
 export default App;
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, {useState, useCallback} from 'react'
 
 const useHttp = () => {
-  return (
-    <div>useHttp</div>
-  )
-}
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, applyData) => {
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : "GET",
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error("Request Failed");
+      }
+
+      const data = await response.json();
+
+      applyData(data);
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
+  }, []);
+
+  return {
+    sendRequest,
+    error,
+  };
+};
 
 export default useHttp
